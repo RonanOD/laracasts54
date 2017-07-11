7ef0e5987d6d67ba3109fafc7da29b3e2468a8d8
w0404884 <Ronan.O'Driscoll@nscc.ca>
2017-07-11 11:52:06 -0300

    Laracast Ep 12

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -24,11 +24,11 @@ class PostsController extends Controller
 //        $post = new Post;
 //        $post->title = request('title');
 //        $post->body = request('body');
-        $post = Post::create([
-            'title' => request('title'),
-            'body' => request('body')
+        $this->validate(request(), [
+            'title' => 'bail|required|unique:posts|max:255',
+            'body' => 'required'
         ]);
-        $post->save();
+        Post::create(request(['title', 'body']));
         return redirect('/');
     }
 }
--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -7,14 +7,17 @@
         {{csrf_field()}}
         <div class="form-group">
             <label for="title">Title:</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" >
         </div>
         <div class="form-group">
             <label for="body">Body</label>
-            <textarea class="form-control" id="body" name="body"></textarea>
+            <textarea class="form-control" id="body" name="body" ></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">
-            Publish
-        </button>
+        @include('layouts.errors')
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">
+                Publish
+            </button>
+        </div>
     </form>
 @endsection
