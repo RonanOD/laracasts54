32d27b7518a3e336c5c1d8255dc2bb2597180924
w0404884 <Ronan.O'Driscoll@nscc.ca>
2017-07-12 14:50:47 -0300

    Laracast Ep 15 Comments

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -8,4 +8,8 @@ class Post extends Model
 {
     // Allow Mass Assignment
     protected $fillable = ['title', 'body'];
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 }
--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -2,5 +2,19 @@
 
 @section('content')
   <h1>{{ $post->title }}</h1>
-  {{ $post->body }}
+
+    {{ $post->body }}
+
+    <hr/>
+
+    <div class="comments">
+        <ul class="list-group">
+        @foreach($post->comments as $comment)
+            <li class="list-group-item">
+                <strong> {{ $comment->created_at->diffForHumans() }}:&nbsp;</strong>
+                {{ $comment->body }}
+            </li>
+        @endforeach
+        </ul>
+    </div>
 @endsection
