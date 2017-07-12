be9ee49141d2ab7b209b0ddf1e9be794d350f516
w0404884 <Ronan.O'Driscoll@nscc.ca>
2017-07-12 14:07:17 -0300

    Laracast Ep 14

--- blog/resources/views/layouts/master.blade.php
+++ blog/resources/views/layouts/master.blade.php
@@ -15,7 +15,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
     <!-- Custom styles for this template -->
-    <link href="/css/blog.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
 <body>
