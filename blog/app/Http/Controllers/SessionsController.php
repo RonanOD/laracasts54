e1262fc58b958af0c027508e461a6326b192411a
w0404884 <Ronan.O'Driscoll@nscc.ca>
2017-11-03 11:30:47 -0300

    Laracast Ep 18 User Assoc 1

---
+++ blog/app/Http/Controllers/SessionsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'destroy']);
+    }
+
+    public function create() {
+        return view('sessions.create');
+    }
+    public function store() {
+        if (!auth()->attempt(request(['email', 'password']))) {
+            return back()->withErrors([
+                'message' => 'Please check your credentials and try again.'
+            ]);
+        }
+        return redirect()->back();
+    }
+    public function destroy() {
+        auth()->logout();
+        return redirect()->home();
+    }
+}
